fb9bd3892d3296365d97f7b7a1ecbff40bade589
Riccardo Cipollone <r.cipollone@softwareindustriale.it>
2024-10-22 16:05:14 +0200

    Added navigation + folder structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,45 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { DxButtonModule } from 'devextreme-angular';
+import { Component, ViewChild } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import {
+  DxDrawerModule,
+  DxListModule,
+  DxRadioGroupModule,
+  DxToolbarModule,
+} from 'devextreme-angular';
+import { List, MockService } from './services/mock.service';
+import { DxDrawerComponent, DxDrawerTypes } from 'devextreme-angular/ui/drawer';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, DxButtonModule],
+  imports: [RouterOutlet, RouterLink, CommonModule, DxToolbarModule, DxDrawerModule, DxListModule, DxRadioGroupModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'demo_devextreme';
-  test() {
-    alert('Zio pera');
-}
+
+  @ViewChild(DxDrawerComponent, { static: false }) drawer!: DxDrawerComponent;
+
+  navigation: List[];
+  text: string;
+  selectedOpenMode: DxDrawerTypes.OpenedStateMode = 'shrink';
+  selectedPosition: DxDrawerTypes.PanelLocation = 'left';
+  selectedRevealMode: DxDrawerTypes.RevealMode = 'slide';
+  isDrawerOpen = true;
+
+  constructor(private service: MockService) {
+    this.text = service.getContent();
+    this.navigation = service.getNavigationList();
+  }
+
+  toolbarContent = [{
+    widget: 'dxButton',
+    location: 'before',
+    options: {
+      icon: 'menu',
+      stylingMode: 'text',
+      onClick: () => this.isDrawerOpen = !this.isDrawerOpen,
+    },
+  }];
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { AgendaComponent } from './features/agenda/agenda.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'home', component: AgendaComponent },
+    { path: 'proposal', component: AgendaComponent },
+    { path: 'contract', component: AgendaComponent },
+    { path: 'personnel', component: AgendaComponent },
+    { path: '', redirectTo: 'home', pathMatch: 'full' }
+];
