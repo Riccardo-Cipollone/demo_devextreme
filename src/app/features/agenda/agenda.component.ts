46bc7b5f5f1517f4469368f70903d8426f6fce05
Riccardo Cipollone <r.cipollone@softwareindustriale.it>
2024-10-22 17:12:59 +0200

    added datagrid test (tutto mock)

--- src/app/features/agenda/agenda.component.ts
+++ src/app/features/agenda/agenda.component.ts
@@ -1,12 +1,20 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { DxDataGridModule } from 'devextreme-angular';
+import { Customer, MockService } from '../../services/mock.service';
 
 @Component({
   selector: 'app-agenda',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, DxDataGridModule],
   templateUrl: './agenda.component.html',
   styleUrl: './agenda.component.scss'
 })
 export class AgendaComponent {
+  customers: Customer[];
+  columns = ['CompanyName', 'City', 'State', 'Phone', 'Fax'];
 
+  constructor(private service: MockService) {
+    this.customers = service.getCustomers();
+  }
 }
