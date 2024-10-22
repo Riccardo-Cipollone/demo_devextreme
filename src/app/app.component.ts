36c11a0f3f1e3fc9baf1adea1b5530c4a2beffad
Riccardo Cipollone <r.cipollone@softwareindustriale.it>
2024-10-22 11:55:14 +0200

    Added devExtreme dependencies (trial)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { DxButtonModule } from 'devextreme-angular';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, DxButtonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'demo_devextreme';
+  test() {
+    alert('Zio pera');
+}
 }
