0881a0bbc50c3a281d9f96b82e12ae1b1b2f8d45
sangelonibei <s.angelonibei@digitality-consulting.com>
2024-10-23 12:39:31 +0200

    added area cell mockup and proposal service

--- src/app/proposal/components/colored-badge.directive.ts
+++ src/app/proposal/components/colored-badge.directive.ts
@@ -6,20 +6,24 @@ import { Directive, HostBinding, Input, OnInit } from '@angular/core';
 })
 export class ColoredBadgeDirective implements OnInit {
   @Input() appColoredBadge!: string;
+  @Input() borderColor!: string;
   @HostBinding('style.backgroundColor') bgColor: string = 'transparent';
   @HostBinding('style.padding') padding: string;
   @HostBinding('style.whiteSpace') whiteSpace: string;
-  @HostBinding('style.color') textColor: string;
+  @HostBinding('style.color') textColor!: string;
   @HostBinding('style.borderRadius') radius: string;
+  @HostBinding('style.border') border: string = '';
 
   constructor() {
     this.padding = '8px 16px';
-    this.textColor = '#fff';
+
     this.radius = '12px';
     this.whiteSpace = 'pre-line';
   }
 
   ngOnInit(): void {
     this.bgColor = this.appColoredBadge;
+    this.textColor = this.borderColor ? '#000' : '#fff';
+    this.border = this.borderColor ? `1px solid ${this.borderColor}` : 'none';
   }
 }
