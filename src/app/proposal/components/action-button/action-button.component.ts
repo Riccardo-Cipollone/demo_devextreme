0aff537bcfb65d6672a9e88ff9cd8795c938911b
sangelonibei <s.angelonibei@digitality-consulting.com>
2024-10-23 10:42:01 +0200

    added temporary header to proposal page

--- src/app/proposal/components/action-button/action-button.component.ts
+++ src/app/proposal/components/action-button/action-button.component.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
+import { NgStyle } from '@angular/common';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-action-button',
   standalone: true,
-  imports: [],
+  imports: [NgStyle],
   templateUrl: './action-button.component.html',
-  styleUrl: './action-button.component.scss'
+  styleUrl: './action-button.component.scss',
 })
 export class ActionButtonComponent {
-
+  @Input() helpText: string = '';
+  @Input() bgColor: string = '';
+  @Input() textColor: string = '#000';
 }
