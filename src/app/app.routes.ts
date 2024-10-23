c76a39f90e5cea3c558a022d329ee1fe2162d167
sangelonibei <s.angelonibei@digitality-consulting.com>
2024-10-23 10:42:01 +0200

    added temporary header to proposal page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,11 @@
 import { Routes } from '@angular/router';
 import { AgendaComponent } from './features/agenda/agenda.component';
+import { ProposalPageComponent } from './proposal/pages/proposal-page/proposal-page.component';
 
 export const routes: Routes = [
-    { path: 'home', component: AgendaComponent },
-    { path: 'proposal', component: AgendaComponent },
-    { path: 'contract', component: AgendaComponent },
-    { path: 'personnel', component: AgendaComponent },
-    { path: '', redirectTo: 'home', pathMatch: 'full' }
+  { path: 'home', component: AgendaComponent },
+  { path: 'proposal', component: ProposalPageComponent },
+  { path: 'contract', component: AgendaComponent },
+  { path: 'personnel', component: AgendaComponent },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
 ];
--- src/app/proposal/pages/proposal-page/proposal-page.component.ts
+++ src/app/proposal/pages/proposal-page/proposal-page.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { DataGridContainerComponent } from '../../components/data-grid-container/data-grid-container.component';
+import { ActionButtonComponent } from '../../components/action-button/action-button.component';
 
 @Component({
   selector: 'app-proposal-page',
   standalone: true,
-  imports: [DataGridContainerComponent],
+  imports: [DataGridContainerComponent, ActionButtonComponent],
   templateUrl: './proposal-page.component.html',
   styleUrl: './proposal-page.component.scss',
 })
