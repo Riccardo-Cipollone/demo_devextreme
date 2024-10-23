b727a48e0c7081397ca5017366e4cacd2f5306bd
sangelonibei <s.angelonibei@digitality-consulting.com>
2024-10-23 16:49:58 +0200

    refactored proposal directory location and removed double icons in drawer

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { AgendaComponent } from './features/agenda/agenda.component';
-import { ProposalPageComponent } from './proposal/pages/proposal-page/proposal-page.component';
+import { ProposalPageComponent } from './features/proposal/pages/proposal-page/proposal-page.component';
+
 
 export const routes: Routes = [
   { path: 'home', component: AgendaComponent },
