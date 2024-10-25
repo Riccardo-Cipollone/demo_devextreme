9190117c030eddda7b59f2ee23c6c6d30be997b8
Riccardo Cipollone <r.cipollone@softwareindustriale.it>
2024-10-25 10:35:47 +0200

    created folder structure for contract page

---
+++ src/app/features/contract/view/components/data-grid-contract/data-grid-contract.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-data-grid-contract',
+  standalone: true,
+  imports: [],
+  templateUrl: './data-grid-contract.component.html',
+  styleUrl: './data-grid-contract.component.scss'
+})
+export class DataGridContractComponent {
+
+}
---
+++ src/app/features/contract/view/contract-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contract-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './contract-page.component.html',
+  styleUrl: './contract-page.component.scss'
+})
+export class ContractPageComponent {
+
+}
