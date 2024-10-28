40ea1593d67e3e9ce2020baa312c9b494192baec
sangelonibei <s.angelonibei@digitality-consulting.com>
2024-10-28 13:06:05 +0100

    added contract to new architecture

--- src/presentation/pages/contract/contract-page.component.ts
+++ src/presentation/pages/contract/contract-page.component.ts
@@ -1,12 +1,54 @@
+import { AsyncPipe } from '@angular/common';
 import { Component } from '@angular/core';
+import { DxLoadPanelModule } from 'devextreme-angular';
+import { EMPTY, Observable, Subscription } from 'rxjs';
+
+import { DataGridContractComponent } from './components/data-grid-contract/data-grid-contract.component';
+import { ActionButtonComponent } from '../proposal/components/action-button/action-button.component';
+
+import { QuartersToolbarComponent } from '../proposal/components/quarters-toolbar/quarters-toolbar.component';
+import { ContractService } from './services/contract.service';
 
 @Component({
   selector: 'app-contract-page',
   standalone: true,
-  imports: [],
+  imports: [
+    DxLoadPanelModule,
+    ActionButtonComponent,
+    AsyncPipe,
+    QuartersToolbarComponent,
+    DataGridContractComponent,
+  ],
   templateUrl: './contract-page.component.html',
-  styleUrl: './contract-page.component.scss'
+  styleUrl: './contract-page.component.scss',
 })
 export class ContractPageComponent {
+  dataSource$: Observable<any[]> = EMPTY;
 
+  startingDate: Date = new Date();
+  isLoading: boolean = false;
+  sourceSubscription!: Subscription;
+  selectedQuarter: string[] = [
+    '7/10',
+    '14/10',
+    '21/10',
+    '28/10',
+    '04/11',
+    '11/11',
+    '18/11',
+    '25/11',
+    '02/12',
+    '09/12',
+    '16/12',
+    '23/12',
+  ];
+  constructor(private proposalService: ContractService) {}
+
+  ngOnInit(): void {
+    this.isLoading = true;
+    this.dataSource$ = this.proposalService.source;
+    this.sourceSubscription = this.dataSource$.subscribe({
+      complete: () => (this.isLoading = false),
+    });
+  }
 }
---
+++ src/presentation/pages/contract/services/contract.service.ts
@@ -0,0 +1,142 @@
+import { Injectable } from '@angular/core';
+import { Observable, of, delay } from 'rxjs';
+import { Project } from '../../../../domain/models/project.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ContractService {
+  private dataSource: any[] = [
+    {
+      figure: 'JR Engineer Italy',
+      actions: [
+        { name: 'add', color: '#00ff00' },
+        { name: 'event', color: '#0000ff' },
+        { name: 'comment', color: '#ff8800' },
+      ],
+      area: null,
+      hours: { tot: 40, scheduled: 40 },
+      '7/10': 15,
+      '14/10': 15,
+      '21/10': 15,
+      '28/10': 15,
+      '04/11': 15,
+      '11/11': 10,
+      '18/11': 0,
+      '25/11': 0,
+      '02/12': 0,
+      '09/12': 0,
+      '16/12': 0,
+      '23/12': 0,
+    },
+    {
+      figure: '',
+      actions: [{ name: 'remove', color: '#ff0000' }],
+      area: 'Civil Eng / Bim',
+      hours: { tot: 40, scheduled: 40 },
+      // '7/10': 15,
+      // '14/10': 15,
+      // '21/10': 15,
+      // '28/10': 15,
+      // '04/11': 15,
+      // '11/11': 10,
+      // '18/11': 0,
+      // '25/11': 0,
+      // '02/12': 0,
+      // '09/12': 0,
+      // '16/12': 0,
+      // '23/12': 0,
+    },
+    {
+      figure: 'JR Engineer India',
+      actions: [
+        { name: 'add', color: '#00ff00' },
+        { name: 'event', color: '#0000ff' },
+        { name: 'comment', color: '#ff8800' },
+      ],
+      area: null,
+      hours: { tot: 40, scheduled: 40 },
+      '7/10': 15,
+      '14/10': 15,
+      '21/10': 15,
+      '28/10': 15,
+      '04/11': 15,
+      '11/11': 10,
+      '18/11': 0,
+      '25/11': 0,
+      '02/12': 0,
+      '09/12': 0,
+      '16/12': 0,
+      '23/12': 0,
+    },
+    {
+      figure: '',
+      actions: [{ name: 'remove', color: '#ff0000' }],
+      area: 'Civil Eng / Con. Safety',
+      hours: { tot: 40, scheduled: 40 },
+      // '7/10': 15,
+      // '14/10': 15,
+      // '21/10': 15,
+      // '28/10': 15,
+      // '04/11': 15,
+      // '11/11': 10,
+      // '18/11': 0,
+      // '25/11': 0,
+      // '02/12': 0,
+      // '09/12': 0,
+      // '16/12': 0,
+      // '23/12': 0,
+    },
+    {
+      figure: 'SR Engineer Italy',
+      actions: [
+        { name: 'add', color: '#00ff00' },
+        { name: 'event', color: '#0000ff' },
+        { name: 'comment', color: '#ff8800' },
+      ],
+      area: null,
+      hours: { tot: 40, scheduled: 40 },
+      '7/10': 15,
+      '14/10': 15,
+      '21/10': 15,
+      '28/10': 15,
+      '04/11': 15,
+      '11/11': 10,
+      '18/11': 0,
+      '25/11': 0,
+      '02/12': 0,
+      '09/12': 0,
+      '16/12': 0,
+      '23/12': 0,
+    },
+  ];
+
+  get source(): Observable<any[]> {
+    var x: Project = {
+      ...new Project(
+        '',
+        [],
+        [],
+        { tot: 40, scheduled: 0 },
+        {
+          '7/10': 15,
+          '14/10': 15,
+          '21/10': 15,
+          '28/10': 15,
+          '04/11': 15,
+          '11/11': 10,
+          '18/11': 0,
+          '25/11': 0,
+          '02/12': 0,
+          '09/12': 0,
+          '16/12': 0,
+          '23/12': 0,
+        }
+      ),
+    };
+
+    console.log(x);
+
+    return of(this.dataSource).pipe(delay(500));
+  }
+}
